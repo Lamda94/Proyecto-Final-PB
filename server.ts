af38fd982e7a229d7336b69b9b70e8958bdb7821
Luis Alberto Mendez de Avila <38790016+Lamda94@users.noreply.github.com>
2021-09-13 21:07:37 -0500

    Desafio clase 14

---
+++ server.ts
@@ -0,0 +1,25 @@
+const handlebars = require("express-handlebars");
+const products = require("./Routes/products.routes.js");
+
+const { ioServer, app, server} = require("./Server/Server");
+
+const ENGINE_NAME:String = "hbs";
+
+app.engine(
+  ENGINE_NAME,
+  handlebars({
+    extname: ".hbs",
+    layoutsDir:"./views/layout",
+    partialsDir:"./views/partials",
+    defaultLayout: "index.hbs",
+  })
+);
+
+app.set("view engine", ENGINE_NAME);
+app.set("views", "./views");
+
+app.use("/api", products);
+
+const PORT:number = 8080;
+server.listen(PORT, ()=>console.log(`App start on http://localhost:8080`));
+server.on("error", (err:object)=>console.log(`Error on server: ${err}`));
