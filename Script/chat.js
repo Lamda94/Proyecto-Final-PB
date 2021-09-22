61dd866934a3bff62d10a47c5a53c5416785b9d0
Luis Alberto Mendez de Avila <38790016+Lamda94@users.noreply.github.com>
2021-09-21 20:40:16 -0500

    bd

--- Script/chat.js
+++ Script/chat.js
@@ -1,38 +1,66 @@
-const {promises} = require('fs');
-const {readFile,writeFile} = promises;
-const url = __dirname.replace("\\Script","");
+const { options } = require("../Script/db.config");
+const knex = require("knex")(options);
 
 class File{ 
-    constructor(name){
-        this.filaName = name;
+    constructor(){  
+        console.log("ingreso");
+        knex.schema
+        .createTable("chat", (table) => {
+            table.increments("id");
+            table.string("email");
+            table.string("menssage");
+            table.timestamp("date").defaultTo(knex.fn.now());
+        })
+        .then(() => console.log("table created"))
+        .catch((error) => {
+            console.log(error);
+            return {manssage:"Error al crear la base de datos.", err:error}
+        })
+        .finally(() => {
+            knex.destroy();
+        });
     }
 
     async getMenssage(){
-        try {
-           const data =  await readFile(this.filaName); 
-           return JSON.parse(data);
-        } catch (error) {
-            return []
-        }        
+      /*  const data = [];
+        knex
+        .from("chat")
+        .select("*")
+        .then((rows) => {
+            for (const row of rows) {
+                data.push({email:row["email"],date:row["date"],menssage:row["menssage"],id:row["id"]});
+            }
+            return data;
+        })
+        .catch((error) => {
+            console.log(error.menssage);
+            return {manssage:"Error al optener los datos de la base de datos.", err:error}
+        })
+        .finally(() => {
+            knex.destroy();
+        });        */
     }
 
-    async addMenssage(data){
-        const array = await this.getMenssage();
-        const dataMensage = {
+    async addMenssage(data){        
+      /*  const dataMensage = [{
             email:data.email, 
             date:data.date, 
             menssage:data.menssage,
-            id: array.length+1,
-        };
-        array.push(dataMensage);
-       try {
-           await writeFile(this.filaName, JSON.stringify(array));
-       } catch (err) {
-           console.log(`Error: ${err.message}`);
-       }        
+        }];
+        knex("chat")
+        .insert(dataMensage)
+        .then(data => {
+            return data; 
+        })
+        .catch((error) => {
+            return {menssage:"Error al insertar los datos", err:error}
+        })
+        .finally(() => {
+            knex.destroy();
+        });        */
     }
 }
 
-const chatMenssage = new File(url+"/data/chat.json");
+const chatMenssage = new File();
 
 module.exports = {chatMenssage};
